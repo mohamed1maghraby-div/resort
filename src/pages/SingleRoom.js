71cba5bc060141edb5f95b72dc7b6f66e96074cd
mohamed1maghraby-div <mohamed1maghraby@yahoo.com>
2021-03-05 18:29:48 +0200

    add Navbar

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+
+class SingleRoom extends Component{
+    render(){
+        return (
+            <div>
+                Single Room
+            </div>
+        );
+    }
+}
+export default SingleRoom;
